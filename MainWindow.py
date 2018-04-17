d4cdc9f407d14a45b29f0eac7106cc69e3a20b67
Viktor Roy <viktor.roy94@mail.ru>
2018-04-17 08:43:32 +0300

    added new functional

--- MainWindow.py
+++ MainWindow.py
@@ -16,6 +16,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.web = QWebEngineView(self.webW)
 
+
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum,
                                            QtWidgets.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -40,20 +41,90 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def createWebPage(self, diam_otver, photo_count):
 
-
         str = "<html>\n"\
               "<meta charset=\"UTF-8\">\n"\
               "<body>\n"
 
+        str += "<style>\n"\
+                    "table {\n"\
+                        "border-spacing: 1px 1px;\n"\
+                        "width: 50;\n" \
+                    "}\n"\
+                    "td {\n"\
+                        "padding: 0px;\n" \
+                        "border: 1px double #333;" \
+                    "}\n" \
+               "</style>\n"
+
         str += "Диаметр отверстия под резьбу - {}\n".format(diam_otver)
         str += "<h3>Инструмент для обработки отверстия под резьбу</h3>\n"
+
+        str += "<table>\n"
+        str += "<tr>\n"
+
+        str += "<td>\n"
         str += "<figure>\n" \
-               "<img src={} alt=\"my img\"/>\n"\
-               "<figcaption> Подпись 1 </figcaption>\n"\
+               "<img style=\"max-width: 160px; height: auto; \" src={} alt=\"my img\"/>\n"\
+               "<figcaption> Сверло </figcaption>\n"\
+               "</figure>\n".format(self.pic.get_sverlo())
+        str += "</td>\n"
+
+        str += "<td>"
+        str += "<figure>\n" \
+               "<img style=\"max-width: 160px; height: auto; \" src={} alt=\"my img\"/>\n" \
+               "<figcaption> Зенкер </figcaption>\n" \
+               "</figure>\n".format(self.pic.get_zenker())
+        str += "</td>\n"
+
+        str += "<td>\n"
+        str += "<figure>\n" \
+               "<img style=\"max-width: 110px; height: auto; \" src={} alt=\"my img\"/>\n" \
+               "<figcaption> Развертка </figcaption>\n" \
+               "</figure>\n".format(self.pic.get_razvertka())
+        str += "</td>\n"
+
+        str += "</tr>\n"
+        str += "</table>\n"
+
+        str += "<h3>Инструмент</h3>\n"
+        str += "<figure>\n" \
+               "<img style=\"max-width: 400px; height: auto; \" src={} alt=\"my img\"/>\n" \
+               "<figcaption> Метчик </figcaption>\n" \
                "</figure>\n".format(self.pic.get_metchik())
 
-        str += "<body>\n"\
-               "<html>"
+        str += "</body>\n"\
+               "</html>"
+
+#         str = ''' <!DOCTYPE html>
+# <html>
+# <head>
+#     <meta charset="UTF-8">
+#     <title></title>
+#     <style >
+#     table {
+#         border: 4px double #333; /* Рамка вокруг таблицы */
+#         border-collapse: separate; /* Способ отображения границы */
+#         width: 25; /* Ширина таблицы */
+#         border-spacing: 1px 2px; /* Расстояние между ячейками */
+#    }
+#    td {
+#     padding: 1px; /* Поля вокруг текста */
+#     border: 1px solid #a52a2a; /* Граница вокруг ячеек */
+#    }
+#     </style>
+# </head>
+# <body>
+#     <table>
+#         <tr>
+#             <td>1</td><td>2</td>
+#         </tr>
+#         <tr>
+#             <td>3</td><td>4</td>
+#         </tr>
+#     </table>
+# </body>
+# </html>'''
+
 
         print(str)
         with io.open("test.html", 'w', encoding="utf-8") as f:
@@ -64,6 +135,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def on_calcB(self):
         fileName = self.createWebPage(0, 2)
         self.web.load(QtCore.QUrl(os.path.abspath(fileName)))
+
 
 
     def on_obrabB(self):
@@ -97,13 +169,13 @@ class Pictures():
         return os.getcwd() + self.folder + self.metchik
 
     def get_patron(self):
-        return self.folder + self.patron
+        return os.getcwd() + self.folder + self.patron
 
     def get_razvertka(self):
-        return self.folder + self.razvertka
+        return os.getcwd() + self.folder + self.razvertka
 
     def get_sverlo(self):
-        return self.folder + self.sverlo
+        return os.getcwd() + self.folder + self.sverlo
 
     def get_zenker(self):
-        return self.folder + self.zenker
+        return os.getcwd() + self.folder + self.zenker
